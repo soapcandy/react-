eaef703b7276a9438655c03941c859d03f228fe4
박지훈 <pjh@ziumks.com>
2024-06-12 15:16:56 +0900

    newsapi API 키 발급받기

--- news-viewers/src/App.jsx
+++ news-viewers/src/App.jsx
@@ -4,22 +4,10 @@ import { useState } from "react";
 const App = () => {
   const [data, setData] = useState(null);
 
-  /* promise 써보기 */
-  // const onClick = () => {
-  //   axios
-  //     .get("http://jsonplaceholder.typicode.com/todos/1") //get 요청
-  //     .then((response) => {
-  //       setData(response.data);
-  //     });
-  // };
-
-  /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
-
-  /* async/await 써보기 */
   const onClick = async () => {
     try {
       const response = await axios.get(
-        "http://jsonplaceholder.typicode.com/todos/1"
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b16364a82fb4060a0c0a421d9dff9ce"
       );
       setData(response.data);
     } catch (e) {
