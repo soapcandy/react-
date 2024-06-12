e999528643bcfe77ca413e2bdb1662cca6b1b613
박지훈 <pjh@ziumks.com>
2024-06-12 09:33:19 +0900

    useEffect 예제 화면 구성

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled, { createGlobalStyle } from "styled-components";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <GlobalStyle />
+      <Post>
+        <Title>Title1</Title>
+        <Body>Body1</Body>
+      </Post>
+      <Post>
+        <Title>Title2</Title>
+        <Body>Body2</Body>
+      </Post>
+      <Post>
+        <Title>Title3</Title>
+        <Body>Body3</Body>
+      </Post>
+      <Post>
+        <Title>Title4</Title>
+        <Body>Body4</Body>
+      </Post>
+    </Container>
+  );
 }
 
-export default App
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+  }
+`;
+
+const Container = styled.div`
+  min-height: 100vh;
+  padding: 200px 0;
+  display: grid;
+  grid-template-columns: repeat(4, 300px);
+  grid-template-rows: repeat(auto-fit, 300px);
+  grid-auto-rows: 300px;
+  grid-gap: 30px 20px;
+  justify-content: center;
+  background: #55efc4;
+  box-sizing: border-box;
+`;
+
+const Post = styled.div`
+  border: 1px solid black;
+  border-radius: 20px;
+  background: white;
+  box-shadow: 10px 5px 5px #7f8fa6;
+`;
+
+const Title = styled.div`
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid black;
+  font-weight: 600;
+`;
+
+const Body = styled.div`
+  height: 80%;
+  padding: 11px;
+  border-radius: 20px;
+`;
+
+export default App;
