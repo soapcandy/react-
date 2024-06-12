33749242b778de15c5e57ec1352c551e7d5e949f
박지훈 <pjh@ziumks.com>
2024-06-12 15:11:15 +0900

    axios로 API 호출해서 데이터 받아오기

--- news-viewers/src/App.jsx
+++ news-viewers/src/App.jsx
@@ -1,11 +1,46 @@
-import Increase from "./components/asynchronous/Promise";
+import axios from "axios";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+
+  /* promise 써보기 */
+  // const onClick = () => {
+  //   axios
+  //     .get("http://jsonplaceholder.typicode.com/todos/1") //get 요청
+  //     .then((response) => {
+  //       setData(response.data);
+  //     });
+  // };
+
+  /* ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ */
+
+  /* async/await 써보기 */
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "http://jsonplaceholder.typicode.com/todos/1"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
   return (
-    <>
-      <Increase />
-    </>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
   );
-}
+};
 
 export default App;
