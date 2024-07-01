d62279193044bd6db403e18d20a0a4ac75e8df19
박지훈 <pjh@ziumks.com>
2024-07-01 09:47:06 +0900

    async방식으로 변경

--- news-viewers2/src/App.jsx
+++ news-viewers2/src/App.jsx
@@ -4,14 +4,15 @@ import { useState } from "react";
 function App() {
   const [data, setData] = useState(null);
 
-  const onClick = () => {
-    axios
-      .get(
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
         `https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b16364a82fb4060a0c0a421d9dff9ce`
-      )
-      .then((response) => {
-        setData(response.data);
-      });
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <div>
