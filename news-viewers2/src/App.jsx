90f9add0266bc4de21f0bf8cce71690cbf4fd9de
박지훈 <pjh@ziumks.com>
2024-07-01 10:12:30 +0900

    news UI 만들기

--- news-viewers2/src/App.jsx
+++ news-viewers2/src/App.jsx
@@ -1,33 +1,19 @@
-import axios from "axios";
-import { useState } from "react";
+import NewsList from "./components/NewsList";
 
 function App() {
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
 
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        `https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b16364a82fb4060a0c0a421d9dff9ce`
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  // const onClick = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `https://newsapi.org/v2/top-headlines?country=kr&apiKey=6b16364a82fb4060a0c0a421d9dff9ce`
+  //     );
+  //     setData(response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
+  return <NewsList />;
 }
 
 export default App;
