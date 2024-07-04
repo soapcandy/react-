5ea14d565c8abafcf6849d8ed7c8551195f05533
박지훈 <pjh@ziumks.com>
2024-07-04 13:05:19 +0900

    개봉일 삭제

--- movie-project/src/components/MovieItem.jsx
+++ movie-project/src/components/MovieItem.jsx
@@ -12,7 +12,6 @@ function MovieItem() {
         {item.movieNm}({item.movieNmEn})
       </div>
       <div className="movie-item-info">제작국가: {item.nationAlt}</div>
-      <div className="movie-item-info">개봉일: {item.openDt}</div>
       <div className="movie-item-info">제작상태: {item.prdtStatNm}</div>
       <div className="movie-item-info">제작연도: {item.prdtYear}</div>
       <div className="movie-item-info">장르명: {item.repGenreNm}</div>
